b9fe1562ca60f2ce6c10e8396cafeb145449176d
davidivanovich <61248102+davidivanovich@users.noreply.github.com>
2020-02-20 00:38:32 +0300

    Привет

--- Основы языка Python/17.02.20/1.py
+++ Основы языка Python/17.02.20/1.py
@@ -4,7 +4,10 @@ pravda = False
 print(number, '=', type(number))
 print(word, '=', type(word))
 print(pravda, '=', type(pravda))
+
 your_number = int(input('Введите ваше число, пожалуйста! - '))
 your_word = input('Введите ваше слово, пожалуйста! - ')
 print(your_number, '- ваше число')
 print(your_word, 'Ваше слово.')
+
+
