7bb24a5b7a8f464fbb36c109553a26f650e3fc5f
David Levashov <davidivanovich33@gmail.com>
2020-02-19 23:15:18 +0300

    Дописал пересчет прибыли на количество сотрудников.

--- Основы языка Python/17.02.20/5.py
+++ Основы языка Python/17.02.20/5.py
@@ -3,6 +3,9 @@ costs = int(input('Введите издержки - '))
 profit = (revenue - costs) / revenue
 if revenue > costs:
     print('Ура! Прибыль! рентабильность выручки составила {}'.format(profit))
+    workers = int(input('Введите колличество сотрудников.'))
+    p_w = (revenue - costs) / workers
+    print('Прибыль в пересчете на работников равна {} на человека'.format(p_w))
 elif revenue == costs:
     print('В этом периоде вы работали в 0.')
 else:
